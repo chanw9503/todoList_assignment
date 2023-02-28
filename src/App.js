0278e60272b7966037207dac05f0a78d437e8323
ChanwooPark <chanw9503@gmail.com>
2023-02-28 17:13:09 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { useState } from 'react'
+
+
 
 function App() {
+
+  const INITIAL_OBJECT = {
+    id : 0,
+    done: false,
+    subject : "초기제목",
+    content : "할일"
+  }
+
+  const [toDoList, setToDoList] = useState([INITIAL_OBJECT]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="root">
+      <div className="layout">
+        <Navigation/>
+        <Add_form/>
+      </div>
     </div>
   );
 }
 
-export default App;
+function Navigation(){
+
+  return(
+    <div className='container'>
+      <div>My Todo List</div>
+      <div>React</div>
+    </div>
+  )
+}
+
+function Add_form() {
+  return (
+    <form className="addForm">
+      <div className="input_group">
+        <lable className="form-label">
+          <strong>제목</strong>
+        </lable>
+        <input required type={"text"} className="form-input" id={"subject_input"} />
+        <label className="form-label">
+          <strong>내용</strong>
+        </label>
+        <input required type={"text"} className="form-input"  id={"content_input"} />
+      </div>
+      <div>
+        <button className='addBtn'>추가하기</button>
+      </div>
+    </form>
+  );
+}
+
+export default App
